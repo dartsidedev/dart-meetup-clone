052679b087433b2e0a601c891ce9e424a2497cff
Vince Varga <vargavince91@gmail.com>
2019-12-01 12:40:13 +0100

    Clean up tests formatting

--- server/test/simple_controller_test.dart
+++ server/test/simple_controller_test.dart
@@ -7,19 +7,43 @@ Future main() async {
     await harness.resetData();
   });
 
-  test("POST /model", () async {
-    final response = await harness.agent.post("/model", body: {"name": "Bob"});
-    expect(response, hasResponse(200, body: {"id": isNotNull, "name": "Bob", "createdAt": isTimestamp}));
+  test('POST /model', () async {
+    final response = await harness.agent.post(
+      '/model',
+      body: {'name': 'Bub'},
+    );
+    expect(
+      response,
+      hasResponse(
+        200,
+        body: {
+          'id': isNotNull,
+          'name': 'Bub',
+          'createdAt': isTimestamp,
+        },
+      ),
+    );
   });
 
-  test("GET /model/:id returns previously created object", () async {
-    var response = await harness.agent.post("/model", body: {"name": "Bob"});
-
-    final createdObject = response.body.as();
-    response = await harness.agent.request("/model/${createdObject["id"]}").get();
+  test('GET /model/:id returns previously created object', () async {
+    final postResponse = await harness.agent.post(
+      '/model',
+      body: {'name': 'Bob'},
+    );
+    final createdObject = postResponse.body.as<Map<String, dynamic>>();
+    final getResponse =
+        await harness.agent.get('/model/${createdObject['id']}');
+    final expectedBody = {
+      'id': createdObject['id'],
+      'name': createdObject['name'],
+      'createdAt': createdObject['createdAt'],
+    };
     expect(
-        response,
-        hasResponse(200,
-            body: {"id": createdObject["id"], "name": createdObject["name"], "createdAt": createdObject["createdAt"]}));
+      getResponse,
+      hasResponse(
+        200,
+        body: expectedBody,
+      ),
+    );
   });
 }
