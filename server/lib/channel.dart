a4c79d40fe78b2d32caae7820e8bd2d73225047e
Vince Varga <vargavince91@gmail.com>
2019-12-01 12:38:35 +0100

    Remove comments from channel.dart
    The removed comments do not provide much value, so better remove it now.

--- server/lib/channel.dart
+++ server/lib/channel.dart
@@ -3,41 +3,25 @@ import 'package:dart_meetup_clone_server/controller/health.dart';
 import 'dart_meetup_clone_server.dart';
 import 'model/model.dart';
 
-/// This type initializes an application.
-///
-/// Override methods in this class to set up routes and initialize services like
-/// database connections. See http://aqueduct.io/docs/http/channel/.
 class DartMeetupCloneServerChannel extends ApplicationChannel {
   ManagedContext context;
 
-  /// Initialize services in this method.
-  ///
-  /// Implement this method to initialize services, read values from [options]
-  /// and any other initialization required before constructing [entryPoint].
-  ///
-  /// This method is invoked prior to [entryPoint] being accessed.
   @override
   Future prepare() async {
     logger.onRecord.listen(
         (rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
 
-    final config =
-        DartMeetupCloneServerConfiguration(options.configurationFilePath);
+    final configFilePath = options.configurationFilePath;
+    final config = DartMeetupCloneServerConfiguration(configFilePath);
     context = contextWithConnectionInfo(config.database);
   }
 
-  /// Construct the request channel.
-  ///
-  /// Return an instance of some [Controller] that will be the initial receiver
-  /// of all [Request]s.
-  ///
-  /// This method is invoked after [prepare].
   @override
   Controller get entryPoint {
     final router = Router();
 
     router
-        .route("/model/[:id]")
+        .route('/model/[:id]')
         .link(() => ManagedObjectController<Model>(context));
 
     router.route('/health').link(() => HealthController());
@@ -45,21 +29,21 @@ class DartMeetupCloneServerChannel extends ApplicationChannel {
     return router;
   }
 
-  /*
-   * Helper methods
-   */
-
   ManagedContext contextWithConnectionInfo(
-      DatabaseConfiguration connectionInfo) {
+    DatabaseConfiguration connectionInfo,
+  ) {
     final dataModel = ManagedDataModel.fromCurrentMirrorSystem();
-    final psc = PostgreSQLPersistentStore(
-        connectionInfo.username,
-        connectionInfo.password,
-        connectionInfo.host,
-        connectionInfo.port,
-        connectionInfo.databaseName);
+    logger.info(
+        'Creating persistent store. Username: ${connectionInfo.username}. Database: ${connectionInfo.databaseName}');
+    final persistentStore = PostgreSQLPersistentStore(
+      connectionInfo.username,
+      connectionInfo.password,
+      connectionInfo.host,
+      connectionInfo.port,
+      connectionInfo.databaseName,
+    );
 
-    return ManagedContext(dataModel, psc);
+    return ManagedContext(dataModel, persistentStore);
   }
 }
 
