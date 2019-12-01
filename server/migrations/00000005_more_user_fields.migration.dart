4f58e198819adc9db5cac0c5526ebae2f3f8d678
Vince Varga <vargavince91@gmail.com>
2019-12-01 20:39:21 +0100

    Format migrations file

--- server/migrations/00000005_more_user_fields.migration.dart
+++ server/migrations/00000005_more_user_fields.migration.dart
@@ -1,18 +1,50 @@
 import 'dart:async';
-import 'package:aqueduct/aqueduct.dart';   
+import 'package:aqueduct/aqueduct.dart';
 
-class Migration5 extends Migration { 
+class Migration5 extends Migration {
   @override
   Future upgrade() async {
-   		database.addColumn("_User", SchemaColumn("description", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: true, isUnique: false));
-		database.addColumn("_User", SchemaColumn("twitter", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: true, isUnique: false));
-		database.addColumn("_User", SchemaColumn("linkedin", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: true, isUnique: false));
+    database.addColumn(
+      "_User",
+      SchemaColumn(
+        "description",
+        ManagedPropertyType.string,
+        isPrimaryKey: false,
+        autoincrement: false,
+        isIndexed: false,
+        isNullable: true,
+        isUnique: false,
+      ),
+    );
+    database.addColumn(
+      "_User",
+      SchemaColumn(
+        "twitter",
+        ManagedPropertyType.string,
+        isPrimaryKey: false,
+        autoincrement: false,
+        isIndexed: false,
+        isNullable: true,
+        isUnique: false,
+      ),
+    );
+    database.addColumn(
+      "_User",
+      SchemaColumn(
+        "linkedin",
+        ManagedPropertyType.string,
+        isPrimaryKey: false,
+        autoincrement: false,
+        isIndexed: false,
+        isNullable: true,
+        isUnique: false,
+      ),
+    );
   }
-  
+
   @override
   Future downgrade() async {}
-  
+
   @override
   Future seed() async {}
 }
-    
