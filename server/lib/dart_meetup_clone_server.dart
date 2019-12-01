8a3e0c83c3bfe0b7d712be898409fc3be67ce71b
Vince Varga <vargavince91@gmail.com>
2019-12-01 09:51:01 +0100

    Create server code from Aqueduct's db template
    aqueduct create -t db dart_meetup_clone_server

---
+++ server/lib/dart_meetup_clone_server.dart
@@ -0,0 +1,11 @@
+/// dart_meetup_clone_server
+///
+/// A Aqueduct web server.
+library dart_meetup_clone_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
---
+++ server/lib/model/model.dart
@@ -0,0 +1,19 @@
+import '../dart_meetup_clone_server.dart';
+
+class Model extends ManagedObject<_Model> implements _Model {
+  @override
+  void willInsert() {
+    createdAt = DateTime.now().toUtc();
+  }
+}
+
+class _Model {
+  @primaryKey
+  int id;
+
+
+  @Column(indexed: true)
+  String name;
+
+  DateTime createdAt;
+}
